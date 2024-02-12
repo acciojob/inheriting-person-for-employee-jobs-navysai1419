8aac59d3c77d1447258ac3c28667c801388a3246
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-12 23:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
 // complete this js code
+class Person{
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	gteet(){
+		console.log(`Hello, my name is ${name}, I am ${age} years old`)
+	}
+}
+class Emploee extends Person {
+	constructor(name,age,jobTitle) {
+		this.name=name;
+		this.age=age;
+		this.jobTitle=jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}`)
+	}
+}
 function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+    }
+}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
 
 function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+
+
